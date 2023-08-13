e626de6f8e273629b03dc5c9dc51908e41953e47
KGESH <seonghyeon.jee@gmail.com>
2023-08-13 17:06:23 +0900

    [Feat] Responsive navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,26 +1,47 @@
 'use client';
+import { useState } from 'react';
 import Link from 'next/link';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="flex flex-row mx-auto w-5/6 mt-5">
-      <div className="flex-1 text-3xl text-gray-100">
-        <Link href="/">Wildeveloper</Link>
-      </div>
-      <ul className="flex flex-row space-x-8">
-        <li className="text-2xl text-gray-100">
-          <Link href="/about">About</Link>
-        </li>
-        <li className="text-2xl text-gray-100">
-          <Link href="/services">Services</Link>
-        </li>
-        <li className="text-2xl text-gray-100">
-          <Link href="/portfolio">Portfolio</Link>
-        </li>
-        <li className="text-2xl text-gray-100">
-          <Link href="/contact">Contact</Link>
-        </li>
+    <div className="relative mb-10">
+      <nav className="flex flex-row mx-auto w-5/6 mt-5">
+        <div className="flex-1 text-3xl text-gray-100 py-1 hover:text-blue-400 transition-colors duration-300">
+          <Link href="/">Wildeveloper</Link>
+        </div>
+
+        {/* Menu icon for mobile */}
+        <button className="lg:hidden px-2 py-1 text-4xl text-gray-100" onClick={() => setIsOpen(!isOpen)}>
+          ☰
+        </button>
+
+        {/* Nav links for desktop */}
+        <ul className="hidden lg:flex space-x-8">
+          {['About', 'Services', 'Portfolio', 'Contact'].map((item) => (
+            <li key={item} className="text-2xl text-gray-100 hover:text-blue-400 transition-colors duration-300">
+              <Link href={`/${item.toLowerCase()}`}>{item}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      {/* Dropdown Nav links for mobile */}
+      <ul
+        className={`absolute top-full mt-2 w-full lg:hidden overflow-hidden transition-max-height duration-300 ${
+          isOpen ? 'max-h-96' : 'max-h-0'
+        } bg-gray-800 shadow-md`}
+      >
+        {['About', 'Services', 'Portfolio', 'Contact'].map((item) => (
+          <li
+            key={item}
+            className="text-2xl text-center text-gray-100 py-1 border-b hover:text-blue-400 transition-colors duration-300"
+          >
+            <Link href={`/${item.toLowerCase()}`}>{item}</Link>
+          </li>
+        ))}
       </ul>
-    </nav>
+    </div>
   );
 }
