fe498e2b09987e7eef76d88549428b4f0a557a26
KGESH <seonghyeon.jee@gmail.com>
2023-08-13 17:00:17 +0900

    [Refactor] extract loading spinner components

--- src/components/map/google-map.tsx
+++ src/components/map/google-map.tsx
@@ -1,12 +1,13 @@
 'use client';
-
 import { useState } from 'react';
 import * as ReactGoogleMap from '@react-google-maps/api';
 import { usePolylineZoomOut } from '@/hooks/use-polyline-zoomout';
-import { Location } from '@/app/(home)/api/service';
-import LoadingSpinner from '@/components/map/loading-spinner';
+import { Location } from '@/components/api/service';
+import MapInfoLoadingSpinner from '@/components/map/map-info-loading-spinner';
+import MapLoadingSpinner from '@/components/map/map-loading-spinner';
+import DistanceInfoWindow from '@/components/map/distance-info-window';
 
-const ownerNickname = 'Wild developer';
+const ownerNickname = 'Wild Developer';
 const clientNickname = 'You';
 
 const redIcon = 'http://maps.google.com/mapfiles/ms/micons/red.png';
@@ -37,11 +38,12 @@ export default function GoogleMap({
   const { isLoaded } = ReactGoogleMap.useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
+    libraries: ['geometry'],
   });
   const [googleMap, setGoogleMap] = useState<google.maps.Map>();
   const onLoad = (map: google.maps.Map) => setGoogleMap(map);
 
-  const { isPolylineLoaded, onPolylineLoad } = usePolylineZoomOut({
+  const { isPolylineLoaded, isInfoLoaded, onPolylineLoad, distanceKm } = usePolylineZoomOut({
     googleMap,
     ownerPos,
     clientPos,
@@ -50,70 +52,75 @@ export default function GoogleMap({
   });
 
   return isLoaded ? (
-    <>
-      <div className="w-4/5" style={{ height: `768px` }}>
-        <ReactGoogleMap.GoogleMap
-          id="google-map"
-          mapContainerStyle={containerStyle}
-          center={centerPos}
-          zoom={6}
-          onLoad={onLoad}
-          options={{ disableDefaultUI: true }}
-        >
+    <div className="w-2/3" style={{ height: `768px` }}>
+      <ReactGoogleMap.GoogleMap
+        id="google-map"
+        mapContainerStyle={containerStyle}
+        center={centerPos}
+        zoom={6}
+        onLoad={onLoad}
+        options={{ disableDefaultUI: true }}
+      >
+        {/*Owner Marker*/}
+        <ReactGoogleMap.MarkerF
+          position={ownerPos}
+          icon={{
+            url: redIcon,
+            labelOrigin: new google.maps.Point(15, -10),
+          }}
+          label={{
+            text: ownerNickname,
+            color: '#C70E20',
+            fontWeight: 'bold',
+          }}
+        ></ReactGoogleMap.MarkerF>
+
+        {/*Client Marker*/}
+        {clientPos ? (
           <ReactGoogleMap.MarkerF
-            position={ownerPos}
+            position={clientPos}
             icon={{
-              url: redIcon,
-              labelOrigin: new google.maps.Point(0, -10),
+              url: blueIcon,
+              labelOrigin: new google.maps.Point(15, -10),
             }}
             label={{
-              text: ownerNickname,
-              color: '#C70E20',
+              text: clientNickname,
+              color: '#0000FF',
               fontWeight: 'bold',
             }}
           ></ReactGoogleMap.MarkerF>
-          {clientPos ? (
-            <ReactGoogleMap.MarkerF
-              position={clientPos}
-              icon={{
-                url: blueIcon,
-                labelOrigin: new google.maps.Point(0, -10),
-              }}
-              label={{
-                text: clientNickname,
-                color: '#0000FF',
-                fontWeight: 'bold',
-              }}
-            ></ReactGoogleMap.MarkerF>
-          ) : (
-            <></>
-          )}
-          {clientPos ? (
-            <ReactGoogleMap.PolylineF
-              onLoad={onPolylineLoad}
-              options={{
-                strokeColor: '#FF0000',
-                strokeOpacity: 1.0,
-                strokeWeight: 2,
-                geodesic: true,
-              }}
-              path={[ownerPos, clientPos]}
-            ></ReactGoogleMap.PolylineF>
-          ) : (
-            <></>
-          )}
-          {isButtonClicked && !isPolylineLoaded && (
-            <ReactGoogleMap.InfoWindowF
-              position={centerPos}
-              options={{ pixelOffset: new window.google.maps.Size(0, -50) }}
-            >
-              <LoadingSpinner />
-            </ReactGoogleMap.InfoWindowF>
-          )}
-        </ReactGoogleMap.GoogleMap>
-      </div>
-    </>
+        ) : (
+          <></>
+        )}
+
+        {/*Owner & client between polyline*/}
+        {clientPos ? (
+          <ReactGoogleMap.PolylineF
+            onLoad={onPolylineLoad}
+            options={{
+              strokeColor: '#FF0000',
+              strokeOpacity: 1.0,
+              strokeWeight: 2,
+            }}
+            path={[ownerPos, clientPos]}
+          ></ReactGoogleMap.PolylineF>
+        ) : (
+          <></>
+        )}
+
+        {/*Polyline info message*/}
+        {isPolylineLoaded && clientPos && (
+          <DistanceInfoWindow distanceKm={distanceKm} ownerPos={ownerPos} clientPos={clientPos} />
+        )}
+
+        {/*Polyline Loading spinner*/}
+        {isButtonClicked && !isPolylineLoaded && <MapInfoLoadingSpinner centerPos={centerPos} />}
+      </ReactGoogleMap.GoogleMap>
+    </div>
   ) : (
-    <></>
+    // Display when google map loading ...
+    <div className="w-2/3" style={{ height: `768px` }}>
+      <MapLoadingSpinner />
+    </div>
   );
 }
