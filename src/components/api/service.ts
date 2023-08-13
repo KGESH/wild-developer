e687cb2ef3e3d4c31f00240b04a9c2bb58fce747
KGESH <seonghyeon.jee@gmail.com>
2023-08-13 17:06:06 +0900

    [Fix] Remove use server

--- src/components/api/service.ts
+++ src/components/api/service.ts
@@ -1,15 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const dynamic = 'force-dynamic';
-
 export type Location = {
   lat: number;
   lng: number;
 };
 
 export const getLatestLocation = async () => {
-  // 'use server';
-
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL as string,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
@@ -25,8 +21,6 @@ export const getLatestLocation = async () => {
 };
 
 export const createLocation = async (location: Location) => {
-  // 'use server';
-
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL as string,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
