0d08f7eae7eb76507683eb860ff72b09e91a8d15
KGESH <seonghyeon.jee@gmail.com>
2023-08-13 14:12:59 +0900

    [Refactor] Modify contact sample page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,21 +1,9 @@
-import { getLatestLocation } from '@/app/(home)/api/service';
-
-export const revalidate = 1; // revalidate the data at most every hour
-
-type Location = {
-  lat: number;
-  lng: number;
-};
-export default async function Contact() {
-  const location = (await getLatestLocation()) as Location;
-
-  return location ? (
+export default function Contact() {
+  return (
     <div>
-      <h1 className="text-7xl font-semibold text-gray-100 mb-4">Lat: {location.lat}</h1> {/*I'm*/}
-      <h2 className="text-4xl font-semibold text-blue-500 mb-4">Lng: {location.lng}</h2>
-      <p className="text-xl text-gray-100 mb-4">{JSON.stringify(location)}</p>
+      <h1 className="text-7xl font-semibold text-gray-100 mb-4">Contact page</h1> {/*I'm*/}
+      <h2 className="text-4xl font-semibold text-blue-500 mb-4">Sample</h2>
+      <p className="text-xl text-gray-100 mb-4">This is sample page</p>
     </div>
-  ) : (
-    <></>
   );
 }
