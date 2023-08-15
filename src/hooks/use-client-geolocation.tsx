7fe226a7942ac82bac69a88911603d4f6e145a22
KGESH <seonghyeon.jee@gmail.com>
2023-08-15 15:41:28 +0900

    [Fix] Add GPS permission query

--- src/hooks/use-client-geolocation.tsx
+++ src/hooks/use-client-geolocation.tsx
@@ -5,11 +5,22 @@ export const useClientGeolocation = () => {
   const [clientLocation, setClientLocation] = useState<Location>();
 
   const getClientLocation = () => {
-    if (navigator.geolocation) {
+    const getGeolocation = () =>
       navigator.geolocation.getCurrentPosition((position) => {
         setClientLocation({
           lat: position.coords.latitude,
           lng: position.coords.longitude,
+        });
+      });
+
+    if (navigator.geolocation) {
+      navigator.permissions.query({ name: 'geolocation' }).then((permission) => {
+        if (permission.state === 'denied') return;
+
+        getGeolocation();
+
+        permission.addEventListener('change', (e) => {
+          if (permission.state === 'granted') getGeolocation();
         });
       });
     } else {
